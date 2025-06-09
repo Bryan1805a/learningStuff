54758c1ce79bfd8dc9c8140e747eac15600c2065
Bryan <buungoc1805@gmail.com>
2025-06-09 14:56:55 +0700

    .

--- pyqt6_learning/app.py
+++ pyqt6_learning/app.py
@@ -1,9 +1,29 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
 import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.button_is_checked = True
+
+        self.setWindowTitle("My App")
+
+        self.button = QPushButton("Press me!")
+        self.button.setCheckable(True)
+        self.button.released.connect(self.the_button_was_released)
+        self.button.setChecked(self.button_is_checked)
+
+        self.setCentralWidget(self.button)
+
+    def the_button_was_released(self):
+        self.button_is_checked = self.button.isChecked()
+        print(self.button_is_checked)
 
 app = QApplication(sys.argv)
 
-window = QMainWindow()
+window = MainWindow()
 window.show()
 
 app.exec()
