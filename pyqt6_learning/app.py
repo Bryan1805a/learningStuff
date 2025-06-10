edabeb653262159ad00f7f73ae3a481dc1ac87fd
Bryan <buungoc1805@gmail.com>
2025-06-10 15:20:38 +0700

    .

--- pyqt6_learning/app.py
+++ pyqt6_learning/app.py
@@ -1,28 +1,33 @@
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QLabel,
-                             QLineEdit, QVBoxLayout, QWidget)
-
 import sys
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QTextEdit
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My app")
 
-        self.label = QLabel()
+        self.setMouseTracking(True)
+        self.label = QLabel("Click in this window")
+        self.label.setMouseTracking(True)
+        self.setCentralWidget(self.label)
 
-        self.input = QLineEdit()
-        self.input.textChanged.connect(self.label.setText)
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        layout.addWidget(self.input)
-
-        container = QWidget()
-        container.setLayout(layout)
-
-        self.setCentralWidget(container)
+    def mousePressEvent(self, e):
+        if e.button() == Qt.MouseButton.LeftButton:
+            self.label.setText("mousePressEvent LEFT")
+        elif e.button() == Qt.MouseButton.MiddleButton:
+            self.label.setText("mousePressEvent MIDDLE")
+        elif e.button() == Qt.MouseButton.RightButton:
+            self.label.setText("mousePressEvent RIGHT")
+    
+    def mouseDoubleClickEvent(self, e):
+        if e.button() == Qt.MouseButton.LeftButton:
+            self.label.setText("mouseDouble LEFT")
 
 app = QApplication(sys.argv)
+
 window = MainWindow()
 window.show()
+
 app.exec()
