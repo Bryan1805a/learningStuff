8082c8a458d572208e3bb9f3592b6150f80258d3
Bryan <buungoc1805@gmail.com>
2025-06-12 15:43:32 +0700

    .

--- pyqt6_learning/app.py
+++ pyqt6_learning/app.py
@@ -1,43 +1,24 @@
 import sys
-
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import (QApplication, QCheckBox, QComboBox,
-                             QDial, QDoubleSpinBox, QLabel,
-                             QLineEdit, QListWidget, QMainWindow,
-                             QSlider, QSpinBox)
+from layout_colorwidget import Color
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget,
+                             QVBoxLayout)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My App")
 
-        widget = QSlider(Qt.Orientation.Horizontal)
+        layout = QVBoxLayout()
 
-        widget.setMinimum(-10)
-        widget.setMaximum(3)
+        layout.addWidget(Color("red"))
+        layout.addWidget(Color("green"))
+        layout.addWidget(Color("orange"))
+        layout.addWidget(Color("blue"))
 
-        widget.setSingleStep(30)
-
-        widget.valueChanged.connect(self.value_changed)
-        widget.sliderMoved.connect(self.slider_position)
-        widget.sliderPressed.connect(self.slider_pressed)
-        widget.sliderReleased.connect(self.slider_released)
-
+        widget = QWidget()
+        widget.setLayout(layout)
         self.setCentralWidget(widget)
 
-    def value_changed(self, i):
-        print(i)
-
-    def slider_position(self, p):
-        print("Pos", p)
-    
-    def slider_pressed(self):
-        print("Pressed!")
-    
-    def slider_released(self):
-        print("Released!")
-
-        
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
--- system32_troll_project/chat_messages.py
+++
@@ -1,7 +0,0 @@
-home_screen_messages = ["Hello!", "Hi!", "Nice!", "Okay."]
-len_HSM = len(home_screen_messages) - 1
-print(len_HSM)
-
-game_screen_messages = ["Okay!", "And?", "Escape", "Test"]
-len_GSM = len(game_screen_messages) - 1
-print(len_GSM)
