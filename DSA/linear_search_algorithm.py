ee1a4a968718d2b8001add88e64c0bb597b9f63f
Bryan <buungoc1805@gmail.com>
2025-06-12 15:26:29 +0700

    .

--- DSA/linear_search_algorithm.py
+++ DSA/linear_search_algorithm.py
@@ -1,3 +1,15 @@
+# Given an array, arr of n integers, 
+# and an integer element x, 
+# find whether element x is present in the array. 
+# Return the index of the first occurrence of x in the array, 
+# or -1 if it doesn't exist.
+
+# Input: arr[] = [1, 2, 3, 4], x = 3
+# Output: 2
+# Explanation: There is one test case with array as [1, 2, 3 4] 
+# and element to be searched as 3. 
+# Since 3 is present at index 2, the output is 2.
+
 def search(arr, n, x):
     for i in range(n):
         if arr[i] == x:
