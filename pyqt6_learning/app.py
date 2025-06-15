ba99b0e36aee725a2f0c5a5a99fe28d2084eec33
Bryan <buungoc1805@gmail.com>
2025-06-15 14:37:17 +0700

    .

--- pyqt6_learning/app.py
+++ pyqt6_learning/app.py
@@ -1,43 +1,25 @@
 import sys
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton,
                              QDialog, QDialogButtonBox, QVBoxLayout,
-                             QLabel)
+                             QLabel, QMessageBox)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My App")
 
-        button = QPushButton("Press me for a dialog")
+        button = QPushButton("Press me for a dialog!")
         button.clicked.connect(self.button_clicked)
         self.setCentralWidget(button)
 
     def button_clicked(self, s):
-        print("click", s)
+        dlg = QMessageBox(self)
+        dlg.setWindowTitle("I have a question!")
+        dlg.setText("This is a simple dialog")
+        button = dlg.exec()
 
-        dlg = CustomDialog()
-        if dlg.exec():
-            print("Sucess!")
-        else:
-            print("Cancel!")
-
-class CustomDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        self.setWindowTitle("HELLO!")
-
-        QBtn = (QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
-
-        self.button_box = QDialogButtonBox(QBtn)
-        self.button_box.accepted.connect(self.accept)
-        self.button_box.rejected.connect(self.reject)
-
-        layout = QVBoxLayout()
-        message = QLabel("Something happened, is that OK?")
-        layout.addWidget(message)
-        layout.addWidget(self.button_box)
-        self.setLayout(layout)
+        if button == QMessageBox.StandardButton.Ok:
+            print("Okay!")
 
 app = QApplication(sys.argv)
 window = MainWindow()
