67db2d9af037c73195a25449faed93bd91e5ed23
Bryan <buungoc1805@gmail.com>
2025-06-19 10:40:55 +0700

    .

--- calculator_project/main.py
+++ calculator_project/main.py
@@ -1,4 +1,30 @@
-from logic import main
+from PyQt6.QtWidgets import (QMainWindow, QApplication, QWidget,
+                             QLineEdit, QPushButton, QLabel,
+                             QVBoxLayout, QHBoxLayout, QGridLayout)
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Settings
+        self.setWindowTitle("Calculator")
+        self.resize(300, 200)
+
+        # Objects
+        text_box = QLineEdit()
+        text_box.setReadOnly(True)
+
+        buttons = [
+            "7", "8", "9"
+        ]
+        # Layouts
+
+        # Function
+
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
