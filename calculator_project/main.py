4a0b078723cbc889b2512d7043e6c4f58558ee2d
Bryan <buungoc1805@gmail.com>
2025-06-19 14:13:52 +0700

    Changed some miscellaneous thing

--- calculator_project/main.py
+++ calculator_project/main.py
@@ -9,18 +9,79 @@ class MainWindow(QMainWindow):
 
         # Settings
         self.setWindowTitle("Calculator")
-        self.resize(300, 200)
+        self.resize(100, 300)
 
         # Objects
-        text_box = QLineEdit()
-        text_box.setReadOnly(True)
+        self.text_box = QLineEdit()
+        self.text_box.setReadOnly(True)
 
         buttons = [
-            "7", "8", "9"
+            "7", "8", "9", "/",
+            "4", "5", "6", "*",
+            "1", "2", "3", "-",
+            "0", ".", "<", "+"
         ]
+        clear_button = QPushButton("CLEAR")
+        clear_button.clicked.connect(self.button_clicked)
+        result_button = QPushButton("=")
+        result_button.clicked.connect(self.button_clicked)
+
         # Layouts
+        main_layout = QVBoxLayout()
+        button_layout = QGridLayout()
+        button_layout_2 = QHBoxLayout()
+
+        main_layout.addWidget(self.text_box)
+
+        # Set button for button_layout
+        row = 0
+        col = 0
+        for i in buttons:
+            button = QPushButton(i)
+            button_layout.addWidget(button, row, col)
+            button.clicked.connect(self.button_clicked)
+            col += 1
+
+            if col > 3:
+                row += 1
+                col = 0
+        
+        button_layout_2.addWidget(clear_button)
+        button_layout_2.addWidget(result_button)
+
+        
+        main_layout.addLayout(button_layout)
+        main_layout.addLayout(button_layout_2)
+
+        container = QWidget()
+        container.setLayout(main_layout)
+
+        self.setCentralWidget(container)
 
         # Function
+    def button_clicked(self):
+        button = app.sender()
+        text = button.text()
+
+        if text == "=":
+            symbol = self.text_box.text()
+            try:
+                res = eval(symbol)
+                self.text_box.setText(str(res))
+            except Exception as e:
+                self.text_box.setText("Error")
+
+        elif text == "CLEAR":
+            self.text_box.clear()
+
+        elif text == "<":
+            current_text = self.text_box.text()
+            self.text_box.setText(current_text[:-1])
+
+        else:
+            current_text = self.text_box.text()
+            self.text_box.setText(current_text + text)
+
 
 
 if __name__ == "__main__":
