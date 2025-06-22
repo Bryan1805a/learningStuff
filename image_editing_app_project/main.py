b48fa08c43db90a64cb060fe67fb11cdfbb03461
Bryan <buungoc1805@gmail.com>
2025-06-22 16:00:13 +0700

    .

--- image_editing_app_project/main.py
+++ image_editing_app_project/main.py
@@ -9,6 +9,7 @@ class MainApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Image Editing App")
+        self.resize(900, 700)
 
         # --- Widgets ---
         # Image display area
