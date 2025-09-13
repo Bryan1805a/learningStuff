b7163d6913a78a818ac85b9d96bbcfb950fe2ce7
Bryan <buungoc1805@gmail.com>
2025-09-13 10:08:01 +0700

    .

--- test/app.py
+++ test/app.py
@@ -1,3 +1,7 @@
-print("Hello, World!")
-# This is a simple Python script that prints "Hello, World!" to the console.
-# Test
+import numpy as np
+
+array_1 = np.arange(1, 10).reshape((3, 3))
+array_2 = np.arange(11, 20).reshape((3, 3))
+
+
+print(np.stack((array_1, array_2), axis=1))
