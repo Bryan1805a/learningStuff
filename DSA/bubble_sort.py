147cae98dc557eee20f058d22f951935244d7395
Bryan <buungoc1805@gmail.com>
2025-10-30 14:18:32 +0700

    .

--- DSA/bubble_sort.py
+++ DSA/bubble_sort.py
@@ -1,16 +1,14 @@
 def bubble_sort(arr):
     n = len(arr)
-    
     for i in range(n):
-        check_swap = False
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                check_swap = True
-                
-        if check_swap == False:
+        swap = False
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swap = True
+        
+        if swap == True:
             break
-    
     
 if __name__ == "__main__":
     arr = [64, 34, 25, 12, 22, 11, 90]
