afc23b0c997361c2eddf68e309dfb02a44a90096
Bryan <buungoc1805@gmail.com>
2025-10-30 19:36:48 +0700

    .

--- DSA/selection_sort.py
+++ DSA/selection_sort.py
@@ -1,26 +1,16 @@
 def selection_sort(arr):
     n = len(arr)
-    
-    for i in range(n):
-        min_idx = i
-        
+    for i in range(n - 1):
+        min_index = i
         for j in range(i + 1, n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-                
-        arr[i], arr[min_idx] = arr[min_idx], arr[i] 
-
-def print_array(arr):
-    for i in arr:
-        print(i, end=" ")
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+    print("Sorted array: ", arr)
+    
 
 if __name__ == "__main__":
     arr = [64, 25, 12, 22, 11]
-    
-    print("Original array: ", end="")
-    print_array(arr)
-    
+
     selection_sort(arr)
-    
-    print("Sorted array: ", end="")
-    print_array(arr)
