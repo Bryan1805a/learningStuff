c3a3ab8831aa513f18d31ffae65ea761cf1a22f0
Bryan <buungoc1805@gmail.com>
2025-11-01 16:45:45 +0700

    .

---
+++ DSA/quicksort_algorithm.py
@@ -0,0 +1,26 @@
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+
+    return i+1
+
+def quicksort(arr, low=0, high=None):
+    if high is None:
+        high = len(arr) - 1
+    
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quicksort(arr, low, pivot_index-1)
+        quicksort(arr, pivot_index+1, high)
+
+if __name__ == "__main__":
+    arr = [16, 17, 4, 3, 5, 2]
+    quicksort(arr)
+    print(arr)
